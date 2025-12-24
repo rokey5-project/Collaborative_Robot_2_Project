fc2f3378949710e71bb484b8f0c67fc1ea7b89f3
gabmin <rlarkqals@gmail.com>
2025-12-24 17:54:07 +0900

    edit/인식률 저하로 인한 파라미터 수정

--- src/albaro/faceAgeNode.py
+++ src/albaro/faceAgeNode.py
@@ -56,7 +56,7 @@ class FaceAgeNode(Node):
 
           gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-          faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+          faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=15, minSize=(30, 30))
 
           for (x, y, w, h) in faces:
               cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
