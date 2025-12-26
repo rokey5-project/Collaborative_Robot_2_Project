8e156dcbb60b536ed9614eef57876bb27303e1f6
gabmin <rlarkqals@gmail.com>
2025-12-26 12:02:58 +0900

    feature/picking_node 기능 연동

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     entry_points={
         'console_scripts': [
             'face_age_node = albaro.face_age_node:main',
+            'picking_node = albaro.picking_node:main'
         ],
     },
 )
